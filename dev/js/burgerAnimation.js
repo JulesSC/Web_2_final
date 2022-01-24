96a4d422bd12c79692aa73e64977f3f79efb066f
JulesSC <81724365+JulesSC@users.noreply.github.com>
2022-01-23 19:49:12 -0500

    we tried

---
+++ dev/js/burgerAnimation.js
@@ -0,0 +1,8 @@
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+
+gsap.registerPlugin(DrawSVGPlugin);
+
+export const burgerTL = new gsap.timeline({paused:true});
+
+burgerTL.to("#outline",{duration:1, alpha:0});
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,25 @@
-import $ from "jquery";
+// import $ from "jquery";
 
-let $nav = $('nav[data-nav]');
-let isVisible = false;
+// let $nav = $('nav[data-nav]');
+// let isVisible = false;
 
-$(".nav-btns").on("click", function(){
-    console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
+// $(".nav-btns").on("click", function(){
+//     console.log("click");
+//     if(isVisible === false){
+//         $nav.show();
+//         isVisible = true;
+//     }
+//     else{
+//         $nav.hide();
+//         isVisible = false;
+//     }
+// })
+
+import { burgerTL } from "./burgerAnimation";
+
+var burgerButton = document.querySelector("#burger");
+
+burgerButton.addEventListener("click", function(){
+    console.log("burger clicked");
 })
+
